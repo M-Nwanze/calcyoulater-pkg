a5e0f6d2ae70033fb3961e3c88a01d7380acd680
paulfurlan <paulfurlan@circuitzero.com>
2021-08-29 00:40:11 -0500

    added division and multiplication unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -5,3 +5,15 @@ def test_addition():
 
 def test_subtraction():
     assert server.subtraction(10, 3) == 7
+
+def test_multiplication():
+    assert server.multiplication(3, 5) == 20
+
+def test_multiplication():
+    assert server.multiplication(5, 0) == 0
+
+def test_division():
+    assert server.division(10, 5) == 2
+
+def test_division():
+    assert server.division(8, 0) == 0
