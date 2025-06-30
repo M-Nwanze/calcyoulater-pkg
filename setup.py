13668550b8390d68eb99f1305c8da1b2dd010b34
mikey nwanze <209273540+M-Nwanze@users.noreply.github.com>
2025-06-30 14:27:40 +0000

    Edit setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='calcyoulater2',
-    version='2.0.1',
+    version='2.0.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
